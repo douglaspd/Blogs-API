b4bcfc49b8816ab502aac2bf2735bd4f6984811d
Douglas de Paiva <douglaspaivadomingues@gmail.com>
2024-05-13 11:31:06 -0300

    ' req 03'

--- src/service/service.js
+++ src/service/service.js
@@ -8,12 +8,12 @@ const login = async (email, password) => {
     if (!user || user.password !== password) {
       return { status: 'BAD_REQUEST', data: { message: 'Invalid fields' } };
     }
-    const token = auth.createToken({ id: user.id,
+    const tokenAuth = auth.createToken({ id: user.id,
       email: user.email, 
       displayName: user.display_name,
       expiresIn: '1h',
     });
-    return { status: 'SUCCESS', data: { message: token } };
+    return { status: 'SUCCESS', data: { token: tokenAuth } };
   } catch (error) {
     return { status: 'SERVER_ERROR', data: { message: 'Erro created' },
     };
